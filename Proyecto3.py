909aa4e0321275fac52fe0e551e06e0fc2915d83
Fhzyiotos <106088714+Fhzyiotos@users.noreply.github.com>
2022-07-12 18:15:39 -0500

    Add files via upload

---
+++ Proyecto3.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ![1bd17ea73f8599cdf8e2818a4a0e5ec9.jpg](attachment:1bd17ea73f8599cdf8e2818a4a0e5ec9.jpg)
+
+# # Proyecto 3
+# ## Ecuaciones Diferenciales: Sistema Resorte Masa por método de Runge Kutta  
+#   
+# ### Presentan:  
+# ### Gallegos Velica Aranxa
+# ### Ramírez Ochoa Santiago
+# ### Ruvalcaba Barbaboza Gabriel
+
+# ## Introducción  
+#   
+#   
+# Las ecuaciones diferenciales son una poderosa herramienta que nos permite modelar situaciones físicas de relativa complejidad, resultando así en diversas aplicaciones útiles en industrias desde la electrónica hasta la automotriz.
+# Una de las mas conocidas aplicaciones es el oscilador armónico, estudiado en clase, y de manera mas específica el sistema resorte masa amortiguador. Como tema de este proyecto se planteará una situación del mundo real y se utilizará un método matemático para obtener la solución a los problemas encontrados, para en seguida implementar esta solución a un código de python que permita explotar el potencial de nuestras herramientas de trabajo y así facilitar y eficientar el trabajo.
+
+# ## Contextualización  
+#   
+# El sistema de resorte masa amortiguador consta como indica el nombre de estos 3 elementos, cada cual aportando una función específica. El propósito principal de este tipo de sistema es 
+# 
